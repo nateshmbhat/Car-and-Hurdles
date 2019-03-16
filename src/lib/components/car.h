bf84bb8649b0ee6f3c7365f1e90e1f3ec79710a5
Mohammed Sadiq <sidsmith9@gmail.com>
2019-03-16 18:43:43 +0530

    Added two parts of a car

--- src/lib/components/car.h
+++ src/lib/components/car.h
@@ -12,51 +12,96 @@ class Car
     static int scalex, scaley, scalez;
 
   public:
-    static float getX(){return x ; }
-    static float getY(){return y ; }
-    static float getZ(){return z ; }
+    static float getX() { return x; }
+    static float getY() { return y; }
+    static float getZ() { return z; }
 
     static void goRight()
     {
-        x+=changeAmount;
+        x += changeAmount;
     }
 
     static void goLeft()
     {
-        x-=changeAmount;
+        x -= changeAmount;
     }
 
     static void goForward()
     {
-        z-=changeAmount;
+        z -= changeAmount;
     }
 
     static void goBackward()
     {
-        z+=changeAmount;
+        z += changeAmount;
     }
 
     static void goUp()
     {
-        y+=changeAmount;
+        y += changeAmount;
     }
 
     static void goDown()
     {
-        y-=changeAmount;
+        y -= changeAmount;
+    }
+
+    static void designCar()
+    {
+
+        // Back WindShield  
+        glColor3d(0, 0, 0);
+        glBegin(GL_POLYGON);
+        glVertex3d(12, 12, 20);
+        glVertex3d(-12, 12, 20);
+        glVertex3d(-12, -4, 20);
+        glVertex3d(12, -4, 20);
+        glEnd();
+
+        // Back Look
+        glColor3d(0, 0, 1);
+        glBegin(GL_POLYGON);
+        glVertex3d(15, 15, 20);
+        glVertex3d(-15, 15, 20);
+        glVertex3d(-15, -15, 20);
+        glVertex3d(15, -15, 20);
+        glEnd();
+
+        // Roof
+        glColor3d(0, 0, 1);
+        glBegin(GL_POLYGON);
+        glVertex3d(15, 15, -20);
+        glVertex3d(15, 15, 20);
+        glVertex3d(-15, 15, 20);
+        glVertex3d(-15, 15, -20);
+        glEnd();
+
+        // Car Panoromic View
+        glColor3d(0, 0, 0);
+        glBegin(GL_POLYGON);
+        glVertex3d(0, 15, -8);
+        glVertex3d(14, 15, 0);
+        glVertex3d(14, 15, 18);
+        glVertex3d(-14, 15, 18);
+        glVertex3d(-14, 15, 0);
+        glEnd();
+
+        glFlush();
     }
 
     static void renderCar()
     {
         // @m-zaink change the car to something good looking
         glPushMatrix();
-        glColor3f(0,1,1) ; 
+        glColor3f(0, 1, 1);
         glTranslatef(x, y, z);
-        glScalef(scalex, scaley, scalez);
-        glutWireSphere(10, 50, 50); //assuming this is car.
+        // glScalef(scalex, scaley, scalez);
+        glScalef(0.1, 0.1, 1);
+        // glutWireSphere(10, 50, 50); //assuming this is car.
+        designCar();
         glPopMatrix();
     }
 };
 
-float Car::x = 0, Car::y, Car::z = 300 , Car::changeAmount = 1;
+float Car::x = 0, Car::y, Car::z = 300, Car::changeAmount = 1;
 int Car::scalex = 1, Car::scaley = 1, Car::scalez = 1;
